7ca7e48c90e73c3635fec5544fcfcda2ff2424d5
EslamSa <eslam.saadfciscu@gmail.com>
2017-04-28 02:58:58 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask_wtf import FlaskForm
 from flask import render_template
+from wtforms import StringField, PasswordField
 
 app = Flask(__name__);
 
-@app.route("/")
+class LoginForm(FlaskForm):
+    username = StringField('username')
+    password = PasswordField('password')
+
+@app.route("/index", methods=['GET','POST'])
 def index():
-    return render_template("index.html")
+    index = LoginForm()
+    if index.validate_on_submit():
+        return '<h2> Ok, In principle</h2>'
+    return render_template("index.html", index=index)
 
 if __name__ == '__main__':
     app.run(debug=True)
