ecd90248b44bbf164c2033c11d855d7af3bdc7c0
yousef <yousef.soliman02@gmail.com>
2017-05-03 14:05:24 +0200

    asdfs

--- run.py
+++ run.py
@@ -1,21 +1,11 @@
 from flask import Flask
-from flask_wtf import FlaskForm
 from flask import render_template
-from wtforms import StringField, PasswordField
-from wtforms_validators import InputRequired, Length
 
 app = Flask(__name__);
 
-class LoginForm(FlaskForm):
-    username = StringField('username', validators = [InputRequired('Username Required!')]
-    password = PasswordField('password', validators = [InpurRequired('Password Required!'), Length(min=8,max=64,'Must be between 8 and 64 characters.')
-
-@app.route("/form", methods=['GET','POST'])
-def form():
-    form = LoginForm()
-    if form.validate_on_submit():
-        return '<h2> Ok, In principle</h2>'
-    return render_template("form.html", form=form)
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
