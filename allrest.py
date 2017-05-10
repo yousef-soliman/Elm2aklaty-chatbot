cbe114fca84f7c196157d819d5661d7ee748798a
yousef <yousef.soliman02@gmail.com>
2017-05-10 20:55:07 +0200

    add

--- allrest.py
+++ allrest.py
@@ -2,7 +2,16 @@
 import json
 from pprint import pprint
 import random
+from flask.ext.mysql import MySQL
+
+app.config['MYSQL_DATABASE_USER'] = 'jay'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'jay'
+app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+
 def restaurant():
     rest = [u'كل المطاعم اهي يا عم']
     rest_str = {"allrest": rest, "do": "", "buttons": {}}
-    print(rest_str["allrest"])
+    
