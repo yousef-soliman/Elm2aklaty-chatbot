eec0b0f62ca036cfd9695a5098e791c2fba42a76
yousef <yousef.soliman02@gmail.com>
2017-05-11 07:22:31 +0200

    connect the features

--- run.py
+++ run.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 from flask import render_template
 from werkzeug.utils import secure_filename
 from flaskext.mysql import MySQL
 import random
+from os import listdir
+from os.path import isfile, join
+
+
 app = Flask(__name__)
 
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = '12345'
-app.config['MYSQL_DATABASE_DB'] = 'restaurant'
+app.config['MYSQL_DATABASE_DB'] = 'restaurants'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
@@ -20,20 +24,104 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/getJson",methods=['GET'])
-def getJson():
-    json_string = '{"first_name": "Guido", "last_name":"Rossum"}'
+@app.route("/getrestaurant",methods = ["POST"])
+def getrestaurant():
+    name = request.form['text']
+    data = tuple()
+    print(name)
+    conn.execute("select * from fish where name = '"+name+"';")
+    data = conn.fetchall()
+    conn.execute("select * from cafe where name = '"+name+"';")    
+    data += conn.fetchall()
+    conn.execute("select * from pizza where name = '"+name+"';")    
+    data += conn.fetchall()
+    conn.execute("select * from local where name = '"+name+"';")    
+    data += conn.fetchall()
+    conn.execute("select * from meats where name = '"+name+"';")    
+    data += conn.fetchall()
+    conn.execute("select * from soury where name = '"+name+"';")    
+    data += conn.fetchall()
+    conn.execute("select * from takeway where name = '"+name+"';")    
+    data += conn.fetchall()
+    
+
+    imgs = []
+    for raw in data:
+        name = raw[3]
+        mypath = "static/restaurants/"+ name.encode("utf-8") + "/"
+        for f in listdir(mypath):
+            if isfile(join(mypath, f.encode("utf-8"))):
+                imgs.append(join(mypath, f.encode("utf-8")))
+                print(join(mypath, f.encode("utf-8")))
+    
+    
+    buttons = [
+        {"text":"تمام", "do":"good()"},
+        {"text":"لا", "do":"ok()"}
+        ]
+    json_str = {'path':imgs,"buttons":buttons}
+    
+    return jsonify(json_str)
+
+
+@app.route("/category")
+def category():
+    buttons = [
+        {"text":"اسماك","name":"fish","do":"getcategory(this)"},
+        {"text":"مشويات","name":"meats","do":"getcategory(this)"},
+        {"text":"شعبي","name":"local","do":"getcategory(this)"},
+        {"text":"بيتزا","name":"pizza","do":"getcategory(this)"},
+        {"text":"سوري","name":"soury","do":"getcategory(this)"}
+    ]
+    json_string = {"message":"عايز تاكل ايه","buttons":buttons}
     return jsonify(json_string)
 
-@app.route("/allrestaurants")
-def allrestaurants():
-    conn.execute("select * from fish")
+
+@app.route("/getcategory", methods = ["POST"])
+def getcategory():
+    table = request.form['text']
+    conn.execute("select * from "+table)
     data = conn.fetchall()
     buttons = []
     for raw in data:
         name = raw[0]
-        buttons.append({"name":name, "value":name})
-    json_string = {"message":"","buttons":buttons}
+        buttons.append({"text":name, "name":name,"do":"send(this)"},)
+    json_string = {"message":"دي المطاعم","buttons":buttons}
+    return jsonify(json_string)
+
+
+@app.route("/getmenu")
+def getmenu():
+   pass
+
+
+
+@app.route("/allrestaurants")
+def allrestaurants():
+    data = tuple()
+    tables_name = conn.execute("show tables;")
+    print(tables_name)
+    conn.execute("select * from fish")
+    data = conn.fetchall()
+    conn.execute("select * from cafe")    
+    data += conn.fetchall()
+    conn.execute("select * from pizza")    
+    data += conn.fetchall()
+    conn.execute("select * from local")    
+    data += conn.fetchall()
+    conn.execute("select * from meats")    
+    data += conn.fetchall()
+    conn.execute("select * from soury")    
+    data += conn.fetchall()
+    conn.execute("select * from takeway")    
+    data += conn.fetchall()
+
+
+    buttons = []
+    for raw in data:
+        name = raw[0]
+        buttons.append({"text":name, "name":name,"do":"send(this)"},)
+    json_string = {"message":"دي المطاعم","buttons":buttons}
     return jsonify(json_string)
 
 
@@ -41,13 +129,22 @@ def allrestaurants():
 def wanttoeat():
         msg = [u'عارف مكانك ولا هتتعبنا',u'طيب اشطة حدد مكانك',u'طب يلا عشان تعزمني ']
         randmsg=random.choice(msg)
-        json_string = {"message": randmsg,"do":"locate()",
-                       "buttons":[{"name":"fish","value":u'سمك'},
-                                  {"name":"meat","value":u'لحمة'},
-                                  {"name": "italian", "value": u'ايطالي'}
+        json_string = {"message": randmsg,
+                       "buttons":[
+                           {"text":"حدد المكان","do":"locate()"},
                                   ]
                         }
-        return (json_string["message"])
+        return jsonify(json_string)
+
+@app.route("/valid")
+def valid():
+        json_string = {
+                       "buttons":[
+                           {"text":"تمام", "do":"ok()"},
+                           {"text":"لا", "do":"delete_marker()"}
+                        ]
+                        }
+        return jsonify(json_string)
 
 
 if __name__ == '__main__':
--- static/js/script.js
+++ static/js/script.js
@@ -1,35 +1,190 @@
 function newMessageFromUser(textMessage){
   var ulElement = document.getElementById("chatList");
   var li =document.createElement("LI")
-  li.innerHTML = "<span class='chat-img pull-left'><img src='static/img/user_3.jpg' alt='User Avatar'></span><div class='chat-body clearfix'><div class='header'><strong class='primary-font'>John Doe</strong><small class='pull-right text-muted'><i class='fa fa-clock-o'></i> 12 mins ago</small></div><p>"+textMessage+"</p></div>"
+  li.innerHTML = "<span class='chat-img pull-left'><img class='img usr' src='static/img/user_3.jpg' alt='User Avatar'></span><div class='chat-body clearfix'><div class='header'><strong class='primary-font'>John Doe</strong><small class='pull-right text-muted'><i class='fa fa-clock-o'></i> 12 mins ago</small></div><p>"+textMessage+"</p></div>"
   li.className = "left clearfix";
+  li.scrollIntoView();
   ulElement.appendChild(li);
+  window.scrollTo(0, document.getElementById("chatBody").scrollHeight);
+
 }
 
 function newMessageFromChat(textMessage){
   var ulElement = document.getElementById("chatList");
   var li =document.createElement("LI")
-  li.innerHTML = "<span class='chat-img pull-right'><img src='static/img/user_1.jpg' alt='User Avatar'></span><div class='chat-body clearfix'><div class='header'><strong class='primary-font'>John Doe</strong><small class='pull-right text-muted'><i class='fa fa-clock-o'></i> 12 mins ago</small></div><p>"+textMessage+"</p></div>"
+  li.innerHTML = "<span class='chat-img pull-right'><img class='img usr' src='static/img/user_1.jpg' alt='User Avatar'></span><div class='chat-body clearfix'><div class='header'><strong class='primary-font'>John Doe</strong><small class='pull-right text-muted'><i class='fa fa-clock-o'></i> 12 mins ago</small></div><p>"+textMessage+"</p></div>"
   li.className = "right clearfix";
   ulElement.appendChild(li);
+  //window.scrollTo(0, document.getElementById("chatBody").scrollHeight);
+  li.scrollIntoView();
 }
-function send() {
-  input = document.getElementById("textMessage");
-  var message = input.value;
-  input.value = "";
-  newMessageFromUser(message)
+function send(e) {
+  $.post('/getrestaurant', {
+        text: $(e).text()
+        
+    }).done(function(data) {
+      for(var i in data["path"]){
+          newMessageFromChat("<img class='menu' src = ' "+ data["path"][i] +"' />")
+      }
+      for(var i in data["buttons"]){
+          addButton(data["buttons"][i])
+      }
+    }).fail(function() {
+        alert("d")
+    });
 }
+
 $(document).ready(function(){
-    $("button").click(function(){
+    $("#allrestaurants").click(function(){
+        newMessageFromUser("كل المطاعم");
         $.get("http://localhost:5000/allrestaurants", function(data){
-            alert("Data: " + data);
+            newMessageFromChat(data["message"])
+            for(var i in data["buttons"]){
+              addButton(data["buttons"][i])
+            }
+        });
+    });
+    $("#wanttoeat").click(function(){
+        newMessageFromUser("عايز اكل");
+        $.get("http://localhost:5000/wanttoeat", function(data){
+            newMessageFromChat(data["message"])
+            for(var i in data["buttons"]){
+              addButton(data["buttons"][i])
+            }
         });
     });
 });
-function addButton(str){
+
+function getmenu(){
+  newMessageFromUser($(e).text());
+  $.post('/getmenu', {
+        text: $(e).attr('name')
+        
+    }).done(function(data) {
+      
+    }).fail(function() {
+        alert("d")
+    });
+}
+
+function getcategory(e){
+  newMessageFromUser($(e).text());
+  $.post('/getcategory', {
+        text: $(e).attr('name')
+        
+    }).done(function(data) {
+      newMessageFromChat(data["message"])
+      for(var i in data["buttons"]){
+          addButton(data["buttons"][i])
+      }
+    }).fail(function() {
+        alert("d")
+    });
+}
+
+
+function addButton(json_str){
   var x = document.getElementsByTagName("UL");
   button = document.createElement("button");
-  button.innerHTML = str;
+  button.innerHTML = json_str["text"];
+  button.setAttribute("onclick",json_str["do"]);
+  button.setAttribute("name",json_str["name"]);
+  button.setAttribute("class","btn button");
   //button.setAttribute("class","tag")
   x[0].appendChild(button);
+  button.scrollIntoView();
 }
+
+function good(){
+  newMessageFromChat("بالهنا و والشفا :D")
+}
+
+function ok(){
+  if(ismarked){
+    $.get("http://localhost:5000/category", function(data){
+          newMessageFromChat(data["message"])
+          for(var i in data["buttons"]){
+            addButton(data["buttons"][i])
+          }
+    });
+  }
+  else{
+    $.get("http://localhost:5000/wanttoeat", function(data){
+            newMessageFromChat(data["message"])
+            for(var i in data["buttons"]){
+              addButton(data["buttons"][i])
+            }
+        });
+  }
+
+}
+
+var marker=null ;
+var ismarked = false;
+var map;
+// var im = 'http://www.robotwoods.com/dev/misc/bluecircle.png';
+function locate(){
+    navigator.geolocation.getCurrentPosition(initialize,fail);
+}
+
+function initialize(position) {
+    
+    newMessageFromUser("حدد مكاني");
+    var ulElement = document.getElementById("chatList");
+    var li =document.createElement("LI")
+    li.innerHTML = "<span class='chat-img pull-right'><img class = 'img usr' src='static/img/user_1.jpg' alt='User Avatar'></span><div class='chat-body clearfix'><div class='header'><strong class='primary-font'>John Doe</strong><small class='pull-right text-muted'><i class='fa fa-clock-o'></i> 12 mins ago</small></div><div id='map_canvas' style = 'width:inherit;height:300px;'></div></div>"
+    li.className = "right clearfix";
+    ulElement.appendChild(li);
+    li.scrollIntoView()
+    if(position == undefined) {
+      var myLatLng = new google.maps.LatLng(30.595227, 32.268501);
+    }
+    else {
+      var myLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    }
+    var mapOptions = {
+      zoom: 17,
+      center: myLatLng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    map = new google.maps.Map(document.getElementById('map_canvas'),
+                                  mapOptions);
+      $.get("http://localhost:5000/valid", function(data){
+          for(var i in data["buttons"]){
+            addButton(data["buttons"][i])
+          }
+      });
+
+
+
+
+    map.addListener('click', function(e) {
+        placeMarker(e.latLng, map);
+    });
+
+    function placeMarker(position, map) {
+      if(!ismarked){
+          marker = new google.maps.Marker({
+          position: position,
+          map: map
+        });
+        map.panTo(position);
+        ismarked = true;
+      }
+    }
+}
+ 		   
+function delete_marker(){
+  if(ismarked){
+      ismarked=false; 
+      marker.setMap(null);
+  }
+}
+
+function fail(){
+    var a;
+     alert('navigator.geolocation failed, may not be supported');
+     initialize(a);
+}
+
+
